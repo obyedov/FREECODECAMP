1e3b1661c6dc28645cc808f2718f1d175d888b98
obyedov <obyedov@gmail.com>
2022-08-02 21:28:21 +0200

    add 21-30 lessons of basicJS

---
+++ 21-30_basicJS.js
@@ -0,0 +1,61 @@
+//Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Modifica il codice solo sotto questa riga
+a += 12;
+b += 9;
+c += 7;
+
+//Assegnazione composta con sottrazione aumentata
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Modifica il codice solo sotto questa riga
+a -= 6;
+b -= 15;
+c -= 1;
+
+//Assegnazione composta con moltiplicazione aumentata
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Modifica il codice solo sotto questa riga
+a *= 5;
+b *= 3;
+c *= 10;
+
+//Assegnazione composta con divisione aumentata
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Modifica il codice solo sotto questa riga
+a /= 12;
+b /= 4;
+c /= 11;
+
+//Fare l'escaping delle virgolette nelle stringhe
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+// Modifica questa riga
+
+//Definire le stringhe con virgolette singole
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//Sequenze di escape nelle stringhe
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Modifica questa riga
+
+//Concatenare le stringhe con l'operatore +
+const myStr = "This is the start. " + "This is the end."; // Modifica questa riga
+
+//Concatenare le stringhe con l'operatore +=
+let myStr = 'This is the first sentence. ';
+myStr += 'This is the second sentence.'
+
+//Costruire stringhe con le variabili
+// Modifica il codice solo sotto questa riga
+const myName = "myName";
+const myStr = "My name is " + myName + " and I am well!";
