79035c70bbcf4210fa10cc8ff72de0b38daedb21
obyedov <obyedov@gmail.com>
2022-08-05 22:26:04 +0200

    add 32 lesson of basicJS

---
+++ 32_basicJS.js
@@ -0,0 +1,7 @@
+//Trovare la lunghezza di una stringa
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Modifica il codice solo sotto questa riga
+lastNameLength = lastName.length;
