349fa2fa7edb0a3698218a81fcc80f220d478a74
obyedov <obyedov@gmail.com>
2022-08-06 12:04:54 +0200

    add 33 lesson of basicJS

---
+++ basicJS.js
@@ -0,0 +1,7 @@
+//Usare la notazione a parentesi per trovare il primo carattere in una stringa
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Modifica il codice solo sotto questa riga
+firstLetterOfLastName = lastName[0]; // Modifica questa riga
