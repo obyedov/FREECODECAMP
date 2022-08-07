8b8f7a49aafb3c49d319800a494b8a1c0161e492
obyedov <obyedov@gmail.com>
2022-08-07 21:04:02 +0200

    add 34 lesson of basicJS

--- basicJS.js
+++ basicJS.js
@@ -5,3 +5,12 @@ const lastName = "Lovelace";
 
 // Modifica il codice solo sotto questa riga
 firstLetterOfLastName = lastName[0]; // Modifica questa riga
+
+///Comprendere l'immutabilità delle stringhe
+
+// Setup
+let myStr = "Jello World";
+
+// Modifica il codice solo sotto questa riga
+myStr = "Hello World"; // Modifica questa riga
+// Modifica il codice solo sopra questa riga
